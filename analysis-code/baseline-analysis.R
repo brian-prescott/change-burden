8fd9ed9040656ba4bb277df8ee4cc09d3c8d03a4
brian-precott <prescott.brian.c@gmail.com>
2021-09-07 17:12:17 -0500

    Updated main results

--- analysis-code/baseline-analysis.R
+++ analysis-code/baseline-analysis.R
@@ -12,14 +12,14 @@
 
 # Notes: If this is your initial use of the code (i.e. if initial_run == TRUE),
 # then be warned that the optimization routines take approximately 10 hours
-# combined to run. I suggest using the already generated model solutions
-# datasets provided on GitHub to review the results. Outside of that, the window
-# containing the script should be extended to the edge of the dashed lines for
-# optimal readibility. The window is approximately 80 lines long. It should be
-# noted that I the uasid variable reported is not actually the "uasid" variable
-# provided by USC but rather is the "id" variable created by the Atlanta Fed.
-# The variable was simply renamed for the purposes of the script. This script
-# reflects a major revision from the original draft.
+# combined to run if using Windows. I suggest using the already generated model
+# solutions datasets provided on GitHub to review the results. Outside of that,
+# the window containing the script should be extended to the edge of the dashed
+# lines for optimal readibility. The window is approximately 80 lines long. It
+# should be noted that I the uasid variable reported is not actually the "uasid"
+# variable  provided by USC but rather is the "id" variable created by the
+# Atlanta Fed. The variable was simply renamed for the purposes of the script.
+# This script reflects a major revision from the original draft.
 
 # Cleaning out the environment before running the script
 rm(list = ls())
@@ -47,12 +47,9 @@ library(boot)
 # PARAMETERS #
 #===============================================================================
 # General directory for more robust exporting
-directory <- str_c(
-  Sys.getenv("USERPROFILE"), 
-  "\\PrescottLibrary\\papers\\academic-papers\\burden-cash\\"
-)
+directory <- str_c(Sys.getenv("OLDPWD"), "/change-burden/")
 # Insert the path to the dataset here
-setwd(paste0(directory, "data\\"))
+setwd(paste0(directory, "data/"))
 
 set.seed(1995)
 
@@ -61,18 +58,18 @@ set.seed(1995)
 #===============================================================================
 calc_optimal_tokens <- function(amounts_list, using_tokens, only_20note) {
   compute_optimal_tokens <- function(a) {
-    amount <<- a
+    amount <<- a #nolint
     tokens <- using_tokens
     n_tokens <- length(tokens)
-    
+
     theta_p <- tokens # theta_p refers to the notation used in the paper
     if (only_20note == TRUE) {
       theta_p[theta_p != 20] <- 0
-      
-    } 
-    
-    f.obj <- rep(1, n_tokens * 2)
-    f.constraint <- c(
+
+    }
+
+    f_obj <- rep(1, n_tokens * 2)
+    f_constraint <- c(
       rep(1, n_tokens * 2), # row 1
       theta_p, rep(0, n_tokens), # row 2
       tokens, -tokens, # row 3
@@ -83,46 +80,49 @@ calc_optimal_tokens <- function(amounts_list, using_tokens, only_20note) {
         nrow = 3 + (n_tokens * 2),
         byrow = TRUE
       )
-    
+
     # The equality ensures that the payment and change equal the amount. The
     # 24 >= ensure that no negative token quantities occur.
-    f.direction <- c(">=", ">=", "==", rep(">=", n_tokens * 2))
-    f.rhs <- c(1, amount, amount, rep(0, n_tokens * 2))
-    
+    f_direction <- c(">=", ">=", "==", rep(">=", n_tokens * 2))
+    f_rhs <- c(1, amount, amount, rep(0, n_tokens * 2))
+
     solve_optimal_tokens <- lpSolve::lp(
       direction = "min",
-      objective.in = f.obj,
-      const.mat = f.constraint,
-      const.dir = f.direction,
-      const.rhs = f.rhs,
-      int.vec = 1:length(f.obj),
+      objective.in = f_obj,
+      const.mat = f_constraint,
+      const.dir = f_direction,
+      const.rhs = f_rhs,
+      int.vec = 1:length(f_obj),
       all.bin = FALSE
     )
-    
+
     optimal_number_tokens <- round(solve_optimal_tokens$objval, digits = 0)
-    
+
     optimal_tokens_df <- optimal_number_tokens %>%
       data.frame(
         n_tokens = .,
         a = amount
       ) %>%
       data.frame()
-    
+
     # Adding in a progress bar to keep track of the optimization
-    progress_bar$tick()$print()
-    
+    # progress_bar$tick()$print()
+
     return(optimal_tokens_df)
   }
-  
+
   # Adding this line in for the progress bar
   suppressWarnings(
     progress_bar <- progress_estimated(length(amounts_list))
   )
-  
+
   optimal_tokens_calculations <- amounts_list %>%
-    purrr::map(.f = compute_optimal_tokens) %>%
+    purrr::map(
+      .x = .,
+      .f = compute_optimal_tokens
+    ) %>%
     bind_rows()
-  
+
   return(optimal_tokens_calculations)
 }
 
@@ -168,11 +168,11 @@ merch_sumStats <- list(mean, sd, median, min, max) %>%
           )
         )
       )
-      
+
       setDT(sumS_df)[, c(levels(sumS_df$payee), "payee") :=
                        c(lapply(levels(payee), function(x)
                          as.integer(x == payee)), .(NULL))]
-      
+
       table <- sumS_df[, (ncol(sumS_df) - 7):ncol(sumS_df)] %>%
         summarise_all(.tbl = .,
                       .funs = ~ h(x = ., na.rm = TRUE)) %>%
@@ -197,19 +197,19 @@ sumStat_table <- list(mean, sd, median, min, max) %>%
     .x = .,
     .f = function(h) {
       sumS_df <- tran_df
-      
+
       setDT(sumS_df)[, c(levels(sumS_df$income_), "income_") :=
                        c(lapply(levels(income_), function(x)
                          as.integer(x == income_)), .(NULL))]
-      
+
       setDT(sumS_df)[, c(levels(sumS_df$age_cohort_), "age_cohort_") :=
                        c(lapply(levels(age_cohort_), function(x)
                          as.integer(x == age_cohort_)), .(NULL))]
-      
+
       setDT(sumS_df)[, c(levels(sumS_df$education_), "education_") :=
                        c(lapply(levels(education_), function(x)
                          as.integer(x == education_)), .(NULL))]
-      
+
       table <- sumS_df %>%
         dplyr::select(
           -c(
@@ -243,9 +243,9 @@ sumStat_table <- list(mean, sd, median, min, max) %>%
                            var = "Covariate") %>%
         arrange(Covariate) %>%
         dplyr::rename(h = ".")
-      
+
       return(table)
-      
+
     }
   ) %>%
   purrr::reduce(
@@ -304,12 +304,15 @@ amnt_list <- as.list(tran_df$amnt)
 # We are using parallel computing here to speed up the optimization algos.
 # I am putting in my own number of cores, however, you will need to change it
 # for your machine. If your machine is Windows, then this will not work and
-# you will need to set num_cores == 1 since Windows does not support the 
+# you will need to set num_cores == 1 since Windows does not support the
 # parallel::mclapply() function. A potential work around is to spin up a
 # Windows Subsytem for Linux instance and run the code on there since you can
-# take advantage of a Linux distro.
-num_cores <- 4 
-optimal_tokens_baseline <- amnt_list %>% 
+# take advantage of a Linux distro. The speed gains make it worthwhile. A
+# windows machine will take approx. 10 hours while a Linux machine using
+# parallel computing takes about 10 minutes.
+num_cores <- 6
+
+optimal_tokens_baseline <- amnt_list %>%
   parallel::mclapply(
     X = .,
     FUN = calc_optimal_tokens,
@@ -323,7 +326,7 @@ optimal_tokens_baseline <- amnt_list %>%
     mc.cleanup = TRUE
   )
 
-optimal_tokens_only_20note <- amnt_list %>% 
+optimal_tokens_only_20note <- amnt_list %>%
   parallel::mclapply(
     X = .,
     FUN = calc_optimal_tokens,
@@ -338,17 +341,16 @@ optimal_tokens_only_20note <- amnt_list %>%
   )
 
 optimal_tokens_df <- optimal_tokens_baseline %>%
+  bind_rows() %>%
   mutate(only_20note = "All coins and notes") %>%
   bind_rows(
     x = .,
     y = optimal_tokens_only_20note %>%
+      bind_rows() %>%
       mutate(only_20note = "Only $20 note")
-  )
+  ) %>%
+  as_tibble()
 
-write_rds(
-  x = optimal_tokens_df,
-  file = str_c("optimal_denom_model_solutions_", Sys.Date(), ".rds")
-)
 save(
   list = c("optimal_tokens_df", "stylized_exercise_1"),
   file = str_c("baseline-results_", Sys.Date(), ".RData")
